898397c4c5585a10f0253b6d638c65aae77e6ceb
Shivam Dhiman <156504081+shivam8870@users.noreply.github.com>
2026-03-03 18:44:18 +0530

    Removed Razorpay routes completely

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import {
     userCredits,
-    paymentRazorpay,
-    verifyRazorpay,
     registerUser,
     loginUser,
     paymentStripe,
@@ -15,8 +13,8 @@ const userRouter = express.Router()
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/credits', authUser, userCredits)
-userRouter.post('/pay-razor', authUser, paymentRazorpay)
-userRouter.post('/verify-razor', verifyRazorpay)
+
+
 userRouter.post('/pay-stripe', authUser, paymentStripe)
 userRouter.post('/verify-stripe', authUser, verifyStripe)
 
