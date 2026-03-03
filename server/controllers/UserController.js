3ec19a3783cf7f37104dc5431750850b482cda0e
Shivam Dhiman <156504081+shivam8870@users.noreply.github.com>
2026-03-03 18:29:57 +0530

    Made Razorpay optional

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -82,11 +82,14 @@ const userCredits = async (req, res) => {
 }
 
 // razorpay gateway initialize
-const razorpayInstance = new razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+let razorpay = null;
 
+if (process.env.RAZORPAY_KEY_ID && process.env.RAZORPAY_KEY_SECRET) {
+  razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+  });
+}
 
 // Payment API to add credits
 const paymentRazorpay = async (req, res) => {
